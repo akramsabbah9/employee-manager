009be8e09c0bcb4d8004039fa133607bb940e91e
Akram Sabbah <akramsabbah9@gmail.com>
2021-02-27 01:13:12 -0800

    work on catching nonexistent credentials file

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const { user, pass } = require("./utils/credentials").getCredentials();
 
 
 /* GLOBALS */
-// const connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: user,
-//     password: pass,
-//     database: "employeeDB"
-// });
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: user,
+    password: pass,
+    database: "employeeDB"
+});
 
 /* FUNCTIONS */
 
--- utils/credentials.js
+++ utils/credentials.js
@@ -5,18 +5,27 @@ const inquirer = require("inquirer");
 
 const credentialsPath = path.join(__dirname, "./cred.txt");
 
-// read the username and password from the credentials file.
+// return the username and password from the credentials file
 const getCredentials = () => {
+    // if the file doesn't exist, do credentialsPrompt first, then read credentials
+    if (!fs.existsSync(credentialsPath)) {
+        console.log("Credentials file does not yet exist.");
+        return credentialsPrompt().then(readCredentials);
+    }
+
+    // otherwise, just read the credentials from the existing file
+    return readCredentials();
+};
+
+// read from credentials file and return credentials
+const readCredentials = () => {
     const credentials = JSON.parse(
         fs.readFileSync(credentialsPath, (err, data) => {
             if (err) throw err;
         })
     );
-    //console.log(__dirname, credentials);
-    return { 
-        user: credentials.user,
-        pass: credentials.pass
-    }
+    console.log(__dirname, credentials);
+    return credentials;
 };
 
 // save a username and password to the credentials file
@@ -25,8 +34,8 @@ const saveCredentials = data => fs.writeFileSync(credentialsPath, JSON.stringify
 // prompt user for username and/or password, and save the answers
 const credentialsPrompt = () => {
     console.log("Please enter your MySQL username and password for use with \
-    the Employee Tracker application. They will be stored in utils/cred.txt.");
-    inquirer.prompt([
+the Employee Tracker application. They will be stored in utils/cred.txt.");
+    return inquirer.prompt([
         {
             type: "input",
             name: "user",
